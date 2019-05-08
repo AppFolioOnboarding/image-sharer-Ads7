4a552781f2fce01996c2cb44c4e030d784bf4942
aman <amandeep.singh@appfolio.com>
2019-05-08 10:52:25 -0700

    as - added image url validation

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,4 @@
 class Image < ApplicationRecord
- end
+  IMAGE_URL_REGEX = /\Ahttps?:\/\/[\/|.|\w|\s|-]*\.(jpe?g|gif|png|svg)\z/
+  validates :url, uniqueness: true, presence: true, format: { with: IMAGE_URL_REGEX,  message: 'Invalid image url' }
+end
